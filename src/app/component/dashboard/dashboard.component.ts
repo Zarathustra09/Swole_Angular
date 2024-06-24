2e4fe2af825c01ed197dca5b24a9fbe6593d9e81
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-06-24 16:43:18 +0800

    Dashbaord Update

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
 import {CalendarComponent} from "../calendar/calendar.component";
-import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [CalendarComponent, RouterLink],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  constructor(private router: Router) { }
 
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 }
